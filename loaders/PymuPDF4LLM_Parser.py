987707d6ac67d83485ee91e80340db06c1210aeb
yun2h <dbsgmldl02@naver.com>
2025-03-23 00:03:29 +0900

    chore : add return, except

--- loaders/PymuPDF4LLM_Parser.py
+++ loaders/PymuPDF4LLM_Parser.py
@@ -9,13 +9,18 @@ def change_pdf_to_md():
     markdown_data = []
 
     #folder에 존재하는 file이 없을 때까지 진행
-    for i in os.listdir(folder_path):
-        #2. 불러온 데이터를 markdown으로 변환,저장하기
-        data_path = f"../data/pdf/{i}"
-        # print(f"{i}파일 변환 중")
-        md_text = pymupdf4llm.to_markdown(data_path)
-        markdown_data.append(md_text)
-        # print(f"현재까지 뽑은 데이터 : {markdown_data}")
+    try :
+        for i in os.listdir(folder_path):
+            #2. 불러온 데이터를 markdown으로 변환,저장하기
+            data_path = f"../data/pdf/{i}"
+            # print(f"{i}파일 변환 중")
+
+            md_text = pymupdf4llm.to_markdown(data_path)
+            markdown_data.append(md_text)
+            print(f"[✓] {i} 파일 변환 완료")
+
+    except Exception as e:
+        print('Error!(Pdf 변환 과정)', e)
 
     print('모든 파일 변환 완료!!')
-    print(markdown_data)
+    return markdown_data
