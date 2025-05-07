0eec79bc73536e4487b537731a1a0c3d73a8f1dd
Chaeyeon PC <ycy8585@ajou.ac.kr>
2025-05-07 15:40:11 +0900

    feat: 불공정약관 탐지 템플릿 수정

--- unfair_dectection.py
+++ unfair_dectection.py
@@ -12,10 +12,11 @@ from prompts.prompt_templates import summary_template, unfair_detect_template
 from loaders.html_parser import clean_html
 from retriever.priority_retriever import PrioritizedLawRetriever
 
-
 # 환경변수에서 OpenAI API 키 로드
 load_dotenv('.env')
 api_key = os.getenv("OPENAI_API_KEY")
+
+
 
 # OpenAI 모델 설정
 llm = ChatOpenAI(model="gpt-4o-2024-08-06", api_key=api_key, temperature=0.2,)
@@ -60,7 +61,7 @@ def unfair_dectection(query: str):
     # 관련 법령 문서 검색
     # 벡터 검색으로 query와 관련된 상위의 5개 법령 문서 가져옴
     # 가장 연관 높은 문서 1개에서 law, clause, chapter, section 정보 추출 (프롬프트에 삽입)
-    docs = vectorstore.as_retriever(search_kwargs={"k": 5}).invoke(query)
+    docs = vectorstore.as_retriever(search_kwargs={"k": 10}).invoke(query)
     metadata = docs[0].metadata
     chain = unfair_detect_template | llm
 
@@ -75,6 +76,9 @@ def unfair_dectection(query: str):
     # docs = [doc for doc, _ in results_with_scores]
     # metadata = docs[0].metadata
     # chain = unfair_detect_template | llm
+    law=metadata.get("law", "")
+    clause = metadata.get("clause", "")
+    law_clause = f"{law} {clause}"
 
 # PromptTemplate에 필요한 모든 필드 전달
     # context: 관련 법량 텍스트 전체(합쳐서 전달), question: 분석 대상 조항
@@ -85,7 +89,8 @@ def unfair_dectection(query: str):
         "law": metadata.get("law", ""),
         "chapter": metadata.get("chapter", ""),
         "section": metadata.get("section", ""),
-        "clause": metadata.get("clause", "")
+        "clause": metadata.get("clause", ""),
+        "law_clause": law_clause
     })
 
 # 결과 처리
@@ -104,11 +109,16 @@ def retriever_test(query: str):
     return docs
 
 query="""
-개인정보의 제3자 제공
-1. <개인정보처리자명>은 정보주체의 개인정보를 개인정보의 처리 목적에서 명시한 범위 내에서만 처리하며, 정보주체의 동의, 법률의 특별한 규정 등 개인정보 보호법 제17조 및 제18조에 해당하는 경우에만 개인정보를 제3자에게 제공하고 이 외에는 정보주체의 개인정보를 제3자에게 제공하지 않습니다.
-2. <개인정보처리자명>은 원활한 서비스 제공을 위해 다음의 경우 정보주체의 동의를 얻어 필요 최소한의 범위로만 제공합니다.
-- 제공받는 자: OO업체, 제공 목적: 부가 서비스 제공, 제공항목: 이름, 휴대폰 번호, 생년월일
-- 제공받는자 : 상품 판매업체, 제공 목적: 추가 상품 안내, 제공항목 : 이름, 휴대폰 번호
+
+개인정보 수집·이용 동의
+
+1. 수집·이용 목적 : 신상품 홍보 및 맞춤형 광고, 타깃 마케팅 제공
+2. 수집하는 개인정보의 항목 : 이메일, 휴대전화번호
+3. 보유 및 이용기간 : 1년
+4. 동의 거부권 및 불이익 : 정보주체는 개인정보 수집·이용에 동의하지 않을 권리가 있으며, 동의를 거부할 경우 신상품 홍보 및 맞춤형 광고, 타깃 마케팅 서비스 이용에 제한을 받을 수 있습니다.
+
+위 개인정보를 수집·이용하는 것에 동의합니다.(선택)
+동의함[V]                             동의하지 않음[  ]
 
 """
 # response = retriever_test(query)
